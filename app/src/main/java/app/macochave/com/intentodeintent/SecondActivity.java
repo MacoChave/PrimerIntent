a2d75f8b47717f1e61ca423c1cebae973ca729fc
Marco Chávez <totto_cha@hotmail.com>
2016-04-18 19:30:55 -0600

    Pasar datos de una activity a otra activity

---
+++ app/src/main/java/app/macochave/com/intentodeintent/SecondActivity.java
@@ -0,0 +1,56 @@
+package app.macochave.com.intentodeintent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class SecondActivity extends AppCompatActivity {
+
+    private TextView texto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        texto = (TextView) findViewById(R.id.txtView);
+
+        Intent i = getIntent();
+        if (i.hasExtra("T")){
+            Bundle contenedor = i.getExtras();
+
+            String entrada = contenedor.getString("T");
+            texto.setText(entrada);
+        } else{
+            Toast.makeText(this, "No se capturó el Intent", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_second, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
